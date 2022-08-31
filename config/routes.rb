8b875efa7b65922a89bdbb4e6cedbde80bd3aebc
kamkara <lnclass.kamkara@gmail.com>
2022-08-31 10:24:10 +0000

    Add feed design

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'feed/index'
   get "home", to:'home#index'
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "post#index"
+  get "post", to:"post#index"
+  root "feed#index"
 end
